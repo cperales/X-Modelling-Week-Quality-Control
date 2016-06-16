e957452d784317807b4ba4f8d51eb35a5e6648c1
Carlos Perales <sir.perales@gmail.com>
2016-06-16 13:26:03 +0200

    A new argumente included. The percentile who works as a threshold can be changed. Before, percentile choosed was 20th. Results of cross validation were:
    Success = 0.845869
    Precission (label 1) = 0.908246
    Precission (label 2) = 0.735163
    Recall (label 1) = 0.858888
    Recall (label 2) = 0.818662
    F_Score (label 1) = 0.882877
    F_Score (label 2) = 0.774669

--- average_distance1.m
+++ average_distance1.m
@@ -1,7 +1,13 @@
 %% This classifier compares the average of the distances of the samples
 % from target 1 with the average of the distnces of the samples from target
 % 2
-function target = average_distance1(train_set,sample)
+function target = average_distance1(train_set,sample,per)
+
+if (nargin==2)
+    per=20;
+end
+
+
 cont1 = 0;
 cont2 = 0;
 d1 = [];
@@ -15,18 +21,23 @@ for i=1:size(train_set,1)
         cont2 = cont2 + 1;
     end
 end
+
 % Just take the distances Normalize de distance to get the average
 if cont1<cont2
-    limit = prctile(d1,20);
+    limit = prctile(d1,per);
 else
-    limit = prctile(d2,20);
+    limit = prctile(d2,per);
 end
+
 d1 = d1(d1<limit);
 d2 = d2(d2<limit);
 d1 = sum(d1)/length(d1);
 d2 = sum(d2)/length(d2);
+
 if d1<d2
     target = 1;
 else
     target = 2;
+end
+
 end
