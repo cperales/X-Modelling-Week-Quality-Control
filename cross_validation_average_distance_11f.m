ad4b4ee09a203b00b6b1f28e2913843470b70d2e
Carlos Perales <sir.perales@gmail.com>
2016-06-16 12:37:41 +0200

    Averages values are calculated to the two target classes

--- cross_validation_average_distance_11f.m
+++ cross_validation_average_distance_11f.m
@@ -8,6 +8,8 @@ set = make_sets3(M,4);
 %% Cross validation
 fscore1 = [];
 recall_vector1 = [];
+fscore2 = [];
+recall_vector2 = [];
 precission_vector2 = [];
 precission_vector1 = [];
 success_vector = [];
@@ -55,19 +57,18 @@ for j = 1:4
     fprintf('FP = %i, percentage = %f\n',FP,FP/total)
     fprintf('Success = %f\n',(TP+TN)/(TP+FP+TN+FN))
     fprintf('Precission (label 1) = %f\n',TP/(TP+FP))
-    fprintf('Precission (label 2) = %f\n',TN/(TN+FN))
     fprintf('Recall (label 1) = %f\n',TP/(TP+FN))
     fprintf('F_Score (label 1) = %f\n',2*(TP/(TP+FN)*TP/(TP+FP))/(TP/(TP+FN) + TP/(TP+FP)))
     fprintf('\n')
-    fscore1 = [fscore1 2*(TP/(TP+FN)*TP/(TP+FP))/(TP/(TP+FN) + TP/(TP+FP))];
-    fscore2 = [fscore2 2*recall_vector2(j)*precission_vector2(j)/(precission_vector2(j) + recall_vector2(j))];
     recall_vector1 = [recall_vector1 TP/(TP+FN)];
     recall_vector2 = [recall_vector2 TN/(TN+FP)];
     precission_vector2 = [precission_vector2 TN/(TN+FN)];
     precission_vector1 = [precission_vector1 ,TP/(TP+FP)];
     success_vector = [success_vector (TP+TN)/(TP+FP+TN+FN)];
+    fscore1 = [fscore1 2*(TP/(TP+FN)*TP/(TP+FP))/(TP/(TP+FN) + TP/(TP+FP))];
+    fscore2 = [fscore2 2*recall_vector2(j)*precission_vector2(j)/(precission_vector2(j) + recall_vector2(j))];
 end
-display('Average values')
+fprintf('\nAverage values')
 fprintf('Success = %f\n',mean(sucess_vector))
 fprintf('Precission (label 1) = %f\n',mean(precission_vector1))
 fprintf('Precission (label 2) = %f\n',mean(precission_vector2))
