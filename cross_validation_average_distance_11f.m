4fc0d11990355e07f4f2eca7f00a1f33ba18612a
Carlos Perales <sir.perales@gmail.com>
2016-06-16 12:24:37 +0200

    Ignored .asv files Added mean cross validation percentages

--- cross_validation_average_distance_11f.m
+++ cross_validation_average_distance_11f.m
@@ -1,11 +1,16 @@
 %% This method compares the average distance classifier
 
 %% Optional code
-% M = csvread('data_11x11f.csv');
-% M = sortrows(M,1059);
-% set = make_sets3(M,4);
+M = csvread('data_11x11f.csv');
+M = sortrows(M,1059);
+set = make_sets3(M,4);
 
 %% Cross validation
+fscore1 = [];
+recall_vector1 = [];
+precission_vector2 = [];
+precission_vector1 = [];
+success_vector = [];
 for j = 1:4
     fprintf('Iteration %i\n',mod(j+3,4)+1)
 %% All the data
@@ -54,4 +59,20 @@ for j = 1:4
     fprintf('Recall (label 1) = %f\n',TP/(TP+FN))
     fprintf('F_Score (label 1) = %f\n',2*(TP/(TP+FN)*TP/(TP+FP))/(TP/(TP+FN) + TP/(TP+FP)))
     fprintf('\n')
+    fscore1 = [fscore1 2*(TP/(TP+FN)*TP/(TP+FP))/(TP/(TP+FN) + TP/(TP+FP))];
+    fscore2 = [fscore2 2*recall_vector2(j)*precission_vector2(j)/(precission_vector2(j) + recall_vector2(j))];
+    recall_vector1 = [recall_vector1 TP/(TP+FN)];
+    recall_vector2 = [recall_vector2 TN/(TN+FP)];
+    precission_vector2 = [precission_vector2 TN/(TN+FN)];
+    precission_vector1 = [precission_vector1 ,TP/(TP+FP)];
+    success_vector = [success_vector (TP+TN)/(TP+FP+TN+FN)];
 end
+display('Average values')
+fprintf('Success = %f\n',mean(sucess_vector))
+fprintf('Precission (label 1) = %f\n',mean(precission_vector1))
+fprintf('Precission (label 2) = %f\n',mean(precission_vector2))
+fprintf('Recall (label 1) = %f\n',mean(recall_vector1))
+fprintf('Recall (label 2) = %f\n',mean(recall_vector2))
+fprintf('F_Score (label 1) = %f\n',mean(fscore1))
+fprintf('F_Score (label 2) = %f\n',mean(fscore2))
+fprintf('\n')
